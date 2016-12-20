03ea35ba31d3c16d0e3b7283bd9072ae2e1c2200
samuel-bohman <samuel.bohman@yahoo.com>
2016-12-20 20:41:20 +0100

    First commit.

---
+++ server.R
@@ -0,0 +1,26 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyServer(function(input, output) {
+  
+  # Call the callModule function and save the reactive expression
+  bins <- callModule(module = sidebar, id = "one")
+  
+  output$distPlot <- renderPlot({
+    
+    # Crack open the reactive expression with ()'s in order to access the value
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = bins() + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+  })
+  
+})
